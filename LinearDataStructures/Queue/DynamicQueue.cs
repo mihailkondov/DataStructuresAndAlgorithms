54a95975b6db0fd62b90ce61505db3c7cc0ec80b
mastersho <116218303+mastersho@users.noreply.github.com>
2024-08-06 21:42:02 +0300

    Added DynamicQueue data structure using the doubly linked list

---
+++ LinearDataStructures/Queue/DynamicQueue.cs
@@ -0,0 +1,91 @@
+﻿using LinearDataStructures.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearDataStructures.Queue
+{
+	public class DynamicQueue<T>
+	{
+		private DoublyLinkedList<T> list;
+		private DoublyLinkedListNode<T>? Oldest;
+		private DoublyLinkedListNode<T>? Newest;
+
+        public DynamicQueue()
+        {
+            list = new DoublyLinkedList<T> ();
+			Oldest = list.Head;
+			Newest = list.Head;
+        }
+
+        public DynamicQueue(T data) : this() 
+        {
+			list.Add(data);
+        }
+
+        public int Count {
+			get
+			{
+				return list.Count;
+			}
+		}
+        public void Enqueue(T item)
+		{
+			list.Add(item);
+			if (Count == 1)
+			{
+				Oldest = list.Head;
+				Newest = list.Head;
+			}
+			else
+			{
+				if(Newest == null)
+				{
+					Newest = list.Head;
+				}
+				else
+				{
+					Newest = Newest.Next;
+				}
+			}
+		}
+
+		public T Peek()
+		{
+			return Oldest.Data;
+		}
+
+		public T Dequeue() 
+		{
+			if(Count == 0)
+			{
+				throw new InvalidOperationException("Queue is empty");
+			}
+
+			T result = list[0];
+			list.RemoveAt(0);
+
+			
+
+			if(Count == 0)
+			{
+				Oldest = null;
+				Newest = null;
+			}
+			else if (Count == 1)
+			{
+				Oldest = Newest;
+			}
+			else
+			{
+				Oldest = Oldest.Next;
+			}
+
+			return result;
+		}
+
+	}
+}
---
+++ LinearDataStructuresTest/DynamicQueueTest.cs
@@ -0,0 +1,97 @@
+﻿using LinearDataStructures.Queue;
+
+namespace Tests
+{
+	internal class DynamicQueueTest
+	{
+		DynamicQueue<int> queue;
+		DynamicQueue<int> queueOne;
+		[SetUp]
+		public void SetUp() 
+		{
+			queue = new DynamicQueue<int>();
+			queueOne = new DynamicQueue<int>(100);
+		}
+
+		[Test]
+		public void Constructors()
+		{
+			DynamicQueue<int> queue1 = new DynamicQueue<int>();
+			Assert.That(queue1 != null);
+			Assert.That(queue1.Count == 0);
+
+			DynamicQueue<int> queue2 = new DynamicQueue<int>(1);
+			Assert.That(queue2 != null);
+			Assert.That(queue2.Count == 1);
+		}
+
+		[Test]
+		public void Enqueue() 
+		{
+			queue.Enqueue(100);
+			Assert.That(queue.Count == 1);
+		}
+
+		[Test]
+		public void Peek()
+		{
+			queue.Enqueue(100);
+			int peekValue = queue.Peek();
+			int sameValue = queue.Peek();
+			Assert.That(queue.Peek() == 100);
+			Assert.That(peekValue == 100);
+			Assert.That(sameValue == 100);
+		}
+
+		[Test]
+		public void Dequeue()
+		{
+			int a = queueOne.Dequeue();
+			Assert.That(a ==  100);
+
+			queueOne.Enqueue(100);
+			queueOne.Enqueue(200);
+			queueOne.Enqueue(300);
+
+			a = queueOne.Dequeue();
+			Assert.That(a == 100);
+
+			a = queueOne.Dequeue();
+			Assert.That(a == 200);
+
+			a = queueOne.Dequeue();
+			Assert.That(a == 300);
+
+			Assert.Throws<InvalidOperationException>(
+				() => { var b = queueOne.Dequeue(); }, "Queue is empty");
+
+
+		}
+
+		[Test]
+		public void Count()
+		{
+			Assert.That(queue.Count == 0);
+			Assert.That(queueOne.Count == 1);
+
+			queue.Enqueue(100);
+			Assert.That(queue.Count == 1);
+			queue.Enqueue(200);
+			Assert.That(queue.Count == 2);
+			queue.Enqueue(300);
+			Assert.That(queue.Count == 3);
+
+			queue.Peek();
+			Assert.That(queue.Count == 3);
+
+			queue.Dequeue();
+			Assert.That(queue.Count == 2);
+
+			queue.Dequeue();
+			Assert.That(queue.Count == 1);
+
+			queue.Dequeue();
+			Assert.That(queue.Count == 0);
+		}
+	}
+}
