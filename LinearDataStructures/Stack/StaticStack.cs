91bc59c0af287c526cedb6d9a2c88f726fea2f35
mastersho <116218303+mastersho@users.noreply.github.com>
2024-08-08 15:56:13 +0300

    Implemented and tested a Stack with a dynamic array to hold the data

---
+++ LinearDataStructures/Stack/StaticStack.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearDataStructures.Stack
+{
+	/// <summary>
+	/// Stack implementation using a dynamic array to hold the data
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class StaticStack<T> : IStack<T>
+	{
+		private static readonly int _initialCapacity = 2;
+		private int _capacity = _initialCapacity;
+		private T[] _dataArray = new T[_initialCapacity];
+		private int _top = -1;
+
+		public StaticStack()
+		{
+
+		}
+
+		public StaticStack(T data)
+		{
+			Push(data);
+		}
+
+		public StaticStack(ICollection<T> data)
+		{
+			foreach (var item in data) 
+			{
+				Push(item);
+			}
+		}
+
+		public int Count { get; private set; } = 0;
+
+		/// <summary>
+		/// Cuts the capacity of the data array in half
+		/// </summary>
+        private void Shrink()
+		{
+			T[] temporary = _dataArray;
+			_capacity /= 2;
+			_dataArray = new T[_capacity];
+
+			for (int i = 0; i < temporary.Length; i++)
+			{
+				_dataArray[i] = temporary[i];
+			}
+		}
+
+		/// <summary>
+		/// Doubles the capacity of the data array
+		/// </summary>
+		private void Grow()
+		{
+			T[] temporary = _dataArray;
+			_capacity *= 2;
+			_dataArray = new T[_capacity];
+
+			for (int i = 0; i < temporary.Length; i++)
+			{
+				_dataArray[i] = temporary[i];	
+			}
+		}
+
+        public T Peek()
+		{
+			return _dataArray[_top];
+		}
+
+		public T Pop()
+		{
+			T result = _dataArray[_top];
+			_top--;
+			Count--;
+
+			if(Count * 3 < _capacity)
+			{
+				Shrink();
+			}
+
+			return result;
+		}
+
+		public void Push(T data)
+		{
+			if(Count == _capacity)
+			{
+				Grow();
+			}
+
+			_top++;
+			_dataArray[_top] = data;
+			Count++;
+		}
+	}
+}
---
+++ LinearDataStructuresTest/StaticStackTest.cs
@@ -0,0 +1,63 @@
+﻿using LinearDataStructures.Stack;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+	internal class StaticStackTest
+	{
+		StaticStack<int> stack;
+		[SetUp]
+		public void Setup()
+		{
+			stack = new StaticStack<int>([1, 2, 3, 4]);
+		}
+
+		[Test]
+		public void Push()
+		{
+			StaticStack<int> constructor1Test = new StaticStack<int>();
+			Assert.That(constructor1Test != null);
+			Assert.That(constructor1Test.Count == 0);
+
+			StaticStack<int> constructor2Test = new StaticStack<int>(1);
+			Assert.That(constructor2Test != null);
+			Assert.That(constructor2Test.Peek() == 1);
+			Assert.That(constructor2Test.Count == 1);
+
+			StaticStack<int> constructor3Test = new StaticStack<int>([1, 2, 3, 4]);
+			Assert.That(constructor3Test.Count == 4);
+
+			StaticStack<int> pushTest = new StaticStack<int>();
+			pushTest.Push(1);
+			Assert.That(pushTest.Count == 1);
+			Assert.That(pushTest.Peek() == 1);
+
+			pushTest.Push(2);
+			Assert.That(pushTest.Count == 2);
+			Assert.That(pushTest.Peek() == 2);
+
+
+		}
+
+		[Test]
+		public void Pop()
+		{
+			int top = stack.Pop();
+			Assert.That(top == 4);
+			Assert.That(stack.Count == 3);
+		}
+
+		[Test]
+		public void Peek()
+		{
+			int top = stack.Peek();
+			Assert.That(top == 4);
+			Assert.That(stack.Count == 4);
+		}
+	}
+}
