fcffdbdf4efe274dabf48ff2ce9dfeef902ada7f
mastersho <116218303+mastersho@users.noreply.github.com>
2024-08-11 00:26:47 +0300

    Removed confusing comments

--- LinearDataStructures/Lists/DynamicArray/ImplementingInterfaces/ArrayDynamicICollection.cs
+++ LinearDataStructures/Lists/DynamicArray/ImplementingInterfaces/ArrayDynamicICollection.cs
@@ -6,10 +6,6 @@ namespace LinearDataStructures.Lists.DynamicArray.ImplementingInterfaces
 {
 	public class ArrayDynamicICollection<T> : ArrayDynamicIEnumerable<T>, ICollection<T>
 	{
-		//private T[] array;
-		//public int Count { get => base.Count; protected set => base.Count = value; } = 0;
-		//public int Capacity { get => base.Capacity; protected set => base.Capacity = value; }
-
 		public ArrayDynamicICollection()
 			: base() { }
 
@@ -100,60 +96,6 @@ namespace LinearDataStructures.Lists.DynamicArray.ImplementingInterfaces
 			return true;
 		}
 
-		/*
-		public void Insert(int index, T data)
-		{
-			if (index > this.Count || index < 0)
-			{
-				throw new ArgumentOutOfRangeException();
-			}
-			if (this.Count == _capacity)
-			{
-				this.Grow();
-			}
-
-			for (int i = this.Count; i > index; i--)
-			{
-				_array[i] = _array[i - 1];
-			}
-			_array[index] = data;
-			this.Count++;
-		}
-
-		public void Delete(T data)
-		{
-			int index = -1;
-			for (int i = 0; i < this.Count; i++)
-			{
-				if (_array[i].Equals(data))
-				{
-					index = i;
-					break;
-				}
-			}
-			if (index > -1)
-			{
-				for (int i = index; i < this.Count - 1; i++)
-				{
-					_array[i] = _array[i + 1];
-				}
-				this.Count--;
-				if (Count < _capacity / 3)
-				{
-					Shrink();
-				}
-			}
-		}
-
-		public bool IsEmpty()
-		{
-			if (Count > 0)
-				return false;
-			if (Count == 0)
-				return true;
-			throw new ArgumentOutOfRangeException(paramName: "Count", message: "DynamicArray Count property is negative");
-		}
-		*/
 		private int Search(T data)
 		{
 			for (int i = 0; i < Count; i++)
--- LinearDataStructures/Lists/DynamicArray/ImplementingInterfaces/ArrayDynamicIEnumerable.cs
+++ LinearDataStructures/Lists/DynamicArray/ImplementingInterfaces/ArrayDynamicIEnumerable.cs
@@ -6,7 +6,9 @@ namespace LinearDataStructures.Lists.DynamicArray.ImplementingInterfaces
 	public class ArrayDynamicIEnumerable<T> : IEnumerable<T>
 	{
 		protected int _capacity = 2;
+
 		protected T[] _array;
+
 		public int Count { get; protected set; } = 0;
 		public int Capacity { get => _capacity; protected set => _capacity = value; }
 
@@ -33,87 +35,5 @@ namespace LinearDataStructures.Lists.DynamicArray.ImplementingInterfaces
 		{
 			return this.GetEnumerator();
 		}
-
-		/*
-		public void Add(T data) 
-		{ 
-			if(this.Count >= _capacity)
-			{
-				this.Grow();
-			}
-			_array[Count] = data;
-			this.Count++;
-		}
-		public void Insert(int  index, T data) 
-		{
-            if (index > this.Count || index < 0)
-            {
-				throw new ArgumentOutOfRangeException();
-            }
-            if (this.Count == _capacity)
-			{
-				this.Grow();
-			}
-
-			for(int i = this.Count; i > index; i--)
-			{
-				_array[i] = _array[i - 1];
-			}
-			_array[index] = data;
-			this.Count++;
-		} 
-		public void Delete(T data)
-		{
-			int index = -1;
-			for(int i = 0; i < this.Count; i++)
-			{
-				if (_array[i].Equals(data))
-				{
-					index = i; 
-					break;	
-				}
-			}
-			if(index > -1)
-			{
-				for(int i = index; i < this.Count - 1; i++)
-				{
-					_array[i] = _array[i + 1];
-				}
-				this.Count--;
-				if (Count < _capacity / 3)
-				{
-					Shrink();
-				}
-			}
-		}
-		public int Search(T data) 
-		{
-			for (int i = 0; i < Count; i++) 
-			{
-				if (_array[i].Equals(data))
-				{
-					return i;
-				}
-			}
-
-			return -1;
-		}
-		private void Grow() 
-		{
-			_capacity *= 2;
-		}
-		private void Shrink() 
-		{
-			_capacity /= 2;
-		}
-		public bool IsEmpty() 
-		{
-			if (Count > 0)
-				return false;
-			if (Count == 0)
-				return true;
-			throw new ArgumentOutOfRangeException(paramName: "Count", message:"DynamicArray Count property is negative");
-		}
-		*/
 	}
 }
