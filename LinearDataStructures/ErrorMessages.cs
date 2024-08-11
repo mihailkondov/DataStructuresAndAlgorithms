0f83fb9de435e91cc2bd01a316edcbce1ec9f12b
mastersho <116218303+mastersho@users.noreply.github.com>
2024-08-11 14:58:36 +0300

    Added Error Messages and constants class for testing

---
+++ LinearDataStructures/ErrorMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearDataStructures
+{
+	public static class ErrorMessages
+	{
+		public const string COPY_ARRAY_TOO_SMALL = "Cannot copy an array of length {0} in an array of length {1} at position {2}. A minimum size of {3} is required";
+	}
+}
--- LinearDataStructuresTest/Constants.cs
+++ LinearDataStructuresTest/Constants.cs
@@ -8,7 +8,8 @@ namespace Tests
 {
 	internal static class Constants
 	{
-		public static int[] INTS = new int[] { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600 };
-		public static string[] STRINGS = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen" };
+		public readonly static int[] INTS = new int[] { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600 };
+		public readonly static string[] STRINGS = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen" };
 	}
+
 }
