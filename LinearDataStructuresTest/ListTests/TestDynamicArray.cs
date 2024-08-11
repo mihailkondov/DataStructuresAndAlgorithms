03fa43cb2e888a4a6d1df5984af0c63ef643ac21
mastersho <116218303+mastersho@users.noreply.github.com>
2024-08-11 14:59:16 +0300

    Wrote tests for ICollection

---
+++ LinearDataStructuresTest/ListTests/TestDynamicArray.cs
@@ -0,0 +1,31 @@
+﻿using LinearDataStructures.Lists.DynamicArray.ImplementingInterfaces;
+using static Tests.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.ListTests
+{
+	public class TestDynamicArray
+	{
+
+		[Test]
+		public void ArrayDynamicICollectionBasicFunctionallity()
+		{
+			//Constructor empty
+			ArrayDynamicICollection<int> emptyArray = new ArrayDynamicICollection<int>();
+
+			Assert.DoesNotThrow(() => { ArrayDynamicICollection<int> array = new ArrayDynamicICollection<int>(); });
+			Assert.DoesNotThrow(() => { ArrayDynamicICollection<string> array = new ArrayDynamicICollection<string>(); });
+			Assert.That(emptyArray.Capacity, Is.EqualTo(2));
+			Assert.That(emptyArray.Count, Is.EqualTo(0));
+
+			//Constructor array
+			ArrayDynamicICollection<int> array = new ArrayDynamicICollection<int>(INTS);
+			Assert.That(array.Count == INTS.Length);
+			Assert.That(array.Capacity, Is.EqualTo(INTS.Length));
+		}
+	}
+}
