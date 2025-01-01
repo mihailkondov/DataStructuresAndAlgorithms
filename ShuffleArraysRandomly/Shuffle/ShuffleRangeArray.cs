ec474063b15b82b3c7b2e5aabc1787517c27cb91
mastersho <116218303+mastersho@users.noreply.github.com>
2025-01-01 22:05:01 +0200

    Added shuffling algorithms with a demo project

---
+++ ShuffleArraysRandomly/Shuffle/ShuffleRangeArray.cs
@@ -0,0 +1,74 @@
+﻿namespace Shuffle
+{
+    public static class ShuffleRangeArray
+    {
+        /// <summary>
+        /// Generates a random array of given length without repeating numbers. 
+        /// Works by generating random numbers and adding them to the resulting array as long as they are not repeating, until the given length is reached.
+        /// </summary>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        public static int[] DrawCheckRedraw(int len)
+        {
+            Random random = new Random();
+            int[] numbers = new int[len];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next() % len + 1; // Draw current number
+
+                for (int j = 0; j < i; j++) // Check it is a repeat
+                {
+                    if (numbers[j] == numbers[i])
+                    {
+                        numbers[i] = random.Next() % len + 1;
+                        j = -1; // reset loop
+                    }
+                }
+            }
+            return numbers;
+        }
+
+        /// <summary>
+        /// Generates a random array of given length without repeating numbers. 
+        /// Works by generating a list of numbers from 1 to the required length and then each element from the list is selected at random and added to another list which is then returned as an array.
+        /// </summary>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        public static int[] MoveLists(int len)
+        {
+            Random random = new Random();
+            List<int> numbers = Enumerable.Range(1, 4).ToList();
+            List<int> shuffled = new List<int>();
+            do
+            {
+                int current = random.Next(0, numbers.Count());
+                shuffled.Add(numbers[current]);
+                numbers.RemoveAt(current);
+            } while (numbers.Count > 0);
+
+            return shuffled.ToArray();
+        }
+
+        /// <summary>
+        /// Generates a random array of given length without repeating numbers. 
+        /// Works by creating an array with the numbers from 1 to the required length and then shuffling them with the Modern Fisher-Yates algorithm.
+        /// </summary>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        public static int[] FisherYates(int len)
+        {
+            int[] numbers = Enumerable.Range(1, len).ToArray();
+            Random random = new Random();
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int rnd = random.Next(i, numbers.Length);
+                int tmp = numbers[i];
+                numbers[i] = numbers[rnd];
+                numbers[rnd] = tmp;
+            }
+
+            return numbers;
+        }
+    }
+}
---
+++ ShuffleArraysRandomly/ShuffleTestProject/Program.cs
@@ -0,0 +1,71 @@
+﻿using static Shuffle.ShuffleRangeArray;
+
+namespace ShuffleTestProject
+{
+    /// <summary>
+    /// This tests different methods of permutating the number sequences by testing with a sequence of four.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shuffle my first attempt:");
+            TestShuffleFour(DrawCheckRedraw);
+
+            Console.WriteLine("Shuffle my second attempt:");
+            TestShuffleFour(MoveLists);
+
+            Console.WriteLine("Fischer-Yates shuffle method:");
+            TestShuffleFour(FisherYates);
+        }
+
+        private static void TestShuffleFour(Func<int, int[]> shuffleFunction)
+        {
+            Dictionary<string, int> statistic = new Dictionary<string, int>() {
+                { "1,2,3,4", 0 }
+            ,   { "1,2,4,3", 0 }
+            ,   { "1,3,2,4", 0 }
+            ,   { "1,3,4,2", 0 }
+            ,   { "1,4,2,3", 0 }
+            ,   { "1,4,3,2", 0 }
+            ,   { "2,1,3,4", 0 }
+            ,   { "2,1,4,3", 0 }
+            ,   { "2,3,1,4", 0 }
+            ,   { "2,3,4,1", 0 }
+            ,   { "2,4,1,3", 0 }
+            ,   { "2,4,3,1", 0 }
+            ,   { "3,1,2,4", 0 }
+            ,   { "3,1,4,2", 0 }
+            ,   { "3,2,1,4", 0 }
+            ,   { "3,2,4,1", 0 }
+            ,   { "3,4,1,2", 0 }
+            ,   { "3,4,2,1", 0 }
+            ,   { "4,1,2,3", 0 }
+            ,   { "4,1,3,2", 0 }
+            ,   { "4,2,1,3", 0 }
+            ,   { "4,2,3,1", 0 }
+            ,   { "4,3,1,2", 0 }
+            ,   { "4,3,2,1", 0 }
+            };
+
+            int probes = 10000;
+            int expected = probes / statistic.Count;
+
+            for (int i = 0; i < probes; i++)
+            {
+                int[] numbers = shuffleFunction(4);
+                string result = string.Join(",", numbers);
+                statistic[result]++;
+            }
+
+            Console.WriteLine($"Expected count: {expected}");
+            Console.WriteLine();
+            foreach (KeyValuePair<string, int> kvp in statistic)
+            {
+                int deviation = kvp.Value - expected;
+                Console.WriteLine(kvp.Key + ":\t" + kvp.Value + "\t" + "Deviation from expected: " + deviation);
+            }
+            Console.WriteLine();
+        }
+    }
+}
